b3db7aeac2fa6310409a241132f42371e7bac03c
FarazAhmad-117 <farazahmad31048@gmail.com>
2025-02-05 00:54:26 +0500

    some owrk

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useQuery, useMutation } from "@tanstack/react-query";
 
-function App() {
-  const [count, setCount] = useState(0)
+const posts = [
+  { id: 1, title: "Post 1" },
+  { id: 2, title: "Post 2" },
+];
+
+const App = () => {
+  const postsQuery = useQuery({
+    queryKey: ["posts"],
+    queryFn: () => wait(1000).then(() => [...posts]),
+  });
+
+  // To generate an error
+  // const postsQuery = useQuery({
+  //   queryKey: ["posts"],
+  //   queryFn: () => Promise.reject("Error Message!"),
+  // });
+
+  if (postsQuery.isLoading) return <h1>Loading....</h1>;
+  if (postsQuery.isError) return <pre>{JSON.stringify(postsQuery.error)}</pre>;
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>Learning tanstack</h1>
+    </div>
+  );
+};
+
+function wait(duration: number) {
+  return new Promise((resolve) => setTimeout(resolve, duration));
 }
 
-export default App
+// What could I do in react query ???
+
+// 1 - Query: Getting data from somewhere (e.g get blogs list)
+
+// 2- Mutation: Changing some type of data (e.g. create blog)
+
+export default App;
