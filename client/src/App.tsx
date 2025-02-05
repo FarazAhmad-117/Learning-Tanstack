2c1ed8f4d72d8589961ea796fbcbf375e84fa5f2
FarazAhmad-117 <farazahmad31048@gmail.com>
2025-02-05 11:42:00 +0500

    something is better undone

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,28 @@
-import { useQuery, useMutation } from "@tanstack/react-query";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 
 const posts = [
   { id: 1, title: "Post 1" },
   { id: 2, title: "Post 2" },
 ];
 
+// /posts >>> ["posts"]
+// /posts/2 >>> ["posts", post.id ]
+// /posts?authorId=1 >>> ["posts", { authorId: 1 } ]
+// /posts/2/comments >>> ["posts", post.id , "comments"]
+
 const App = () => {
+  const queryClient = useQueryClient();
+
   const postsQuery = useQuery({
     queryKey: ["posts"],
     queryFn: () => wait(1000).then(() => [...posts]),
+  });
+
+  const postMutation = useMutation({
+    mutationFn: (title: string) => {
+      return wait(1000).then(() => posts.push({ id: posts.length + 1, title }));
+    },
+    onSuccess: () => queryClient.invalidateQueries(["posts"]),
   });
 
   // To generate an error
@@ -22,7 +36,15 @@ const App = () => {
 
   return (
     <div>
-      <h1>Learning tanstack</h1>
+      {postsQuery.data?.map((post) => (
+        <div key={post.id}>{post.title}</div>
+      ))}
+      <button
+        disabled={postMutation.isPending}
+        onClick={() => postMutation.mutate("New Post")}
+      >
+        Add Post
+      </button>
     </div>
   );
 };
